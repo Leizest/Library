955e7c3f8448884a63166e9cda1ff6781562cbaf
Leizest <73422415+Leizest@users.noreply.github.com>
2020-10-25 14:25:29 +0200

    Add files via upload

---
+++ Slot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IPointerEnterHandler
+{
+    public GameObject item;
+    public int ID;
+    public string Type;
+    public string Description;
+    public bool Empty;
+    public Sprite icon;
+    public Transform SlotIconGO;
+    // Start is called before the first frame update
+    public void UpdateSlots()
+    {
+        SlotIconGO.GetComponent<Image>().sprite = icon;
+    }
+    void Start()
+    {
+        SlotIconGO = transform.GetChild(0);
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log(Description);
+    }
+
+    void Update()
+    {
+        
+    }
+}
